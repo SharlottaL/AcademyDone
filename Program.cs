4853b7501d43c68023642cabe6349d7566236041
SharlottaL <botanichkagigantika@gmail.com>
2025-05-24 12:52:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    class Program
+    {
+        static readonly string delimiter = "\n----------------------------------------------\n";
+        static void Main(string[] args)
+        {
+
+#if INHERITANCE
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Info();
+			Console.WriteLine(delimiter);
+
+			Student student = new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 96);
+			student.Info();
+			Console.WriteLine(delimiter);
+
+			Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+			teacher.Info();
+			Console.WriteLine(delimiter);
+
+			Human tommy = new Human("Vercetty", "Tommy", 30);
+			tommy.Info();
+			Console.WriteLine(delimiter);
+
+			Student s_tommy = new Student(tommy, "Theft", "Vice", 95, 98);
+			s_tommy.Info();
+			Console.WriteLine(delimiter);
+
+			Graduate g_tommy = new Graduate(s_tommy, "How to make money");
+			g_tommy.Info();
+			Console.WriteLine(delimiter);
+
+			Graduate graduate = new Graduate("Schreder", "Hank", 40, "Criminalistic", "OBN", 70, 80, "How to catch Heizenberg");
+			graduate.Info();
+			Console.WriteLine(delimiter); 
+#endif
+
+#if SAVE_TO_FILE
+			//Generalization (Обобщение):
+			Human[] group = new Human[]
+				{
+					//Upcast - преобразование объекта дочернего класса в объект базового класса
+					new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 96),
+					new Teacher("White", "Walter", 50, "Chemistry", 25),
+					new Graduate("Vercetty", "Tommy", 30, "Theft", "Vice", 95, 98, "How to make money"),
+					new Graduate("Schreder", "Hank", 40, "Criminalistic", "OBN", 70, 80, "How to catch Heizenberg"),
+					new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
+				};
+
+			Print(group);
+			Save(group, "group.csv"); 
+#endif
+        
+            Human[] group = Load("base.csv");
+            Print(group);
+
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+        }
+        public static void Print(Human[] group)
+        {
+            for (int i = 0; i < group.Length; i++)
+            {
+                Console.WriteLine(group[i]);
+            }
+        }
+        public static void Save(Human[] group, string filename)
+        {
+
+            StreamWriter sw = new StreamWriter(filename);
+
+            for (int i = 0; i < group.Length; i++)
+            {
+                sw.WriteLine(group[i].ToFileString());
+            }
+
+            sw.Close();
+
+            Process.Start("notepad.exe", filename);
+        }
+        public static Human[] Load(string filename)
+        {
+            List<Human> group = new List<Human>();
+
+            try
+            {
+                StreamReader sr = new StreamReader(filename);
+
+                while (!sr.EndOfStream)
+                {
+                    string buffer = sr.ReadLine();
+                    Human human = HumanFactory(buffer.Split(':').First());
+                    human.Init(buffer.Split(':').Last().Split(','));
+                    group.Add(human);
+                }
+
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return group.ToArray();
+        }
+        public static Human HumanFactory(string type)
+        {
+            Human human = null;
+            switch (type)
+            {
+                case "Human": human = new Human("", "", 0); break;
+                case "Student": human = new Student("", "", 0, "", "", 0, 0); break;
+                case "Graduate": human = new Graduate("", "", 0, "", "", 0, 0, "n/a"); break;
+                case "Specialist": human = new Specialist("", "", 0, "", "", 0, 0, "n/a",0.0,"",0); break;
+                case "Teacher": human = new Teacher("", "", 0, "", 0); break;
+            }
+            return human;
+        }
+    }
+}
---
+++ Specialist.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Academy
+{
+    internal class Specialist : Graduate
+    {
+        public double Experience { get; set; }
+        public string SpecialSkills { get; set; }
+        public int Salary { get; set; }
+        public Specialist
+            (
+            string last_name, string first_name, int age,
+            string speciality, string group, double rating, double attendance,
+            string subject,
+            double experience, string special_skills, int salary
+            ) : base(last_name, first_name, age, speciality, group, rating, attendance, subject)
+        {
+            Experience = experience;
+            SpecialSkills = special_skills;
+            Salary = salary;
+            Console.WriteLine($"SConstructor\t:{this.GetHashCode()}");
+        }
+        public Specialist(Graduate graduate, double experience, string special_skills, int salary) : base(graduate)
+        {
+            Experience = experience;
+            SpecialSkills = special_skills;
+            Salary = salary;
+            Console.WriteLine($"SConstructor\t:{this.GetHashCode()}");
+        }
+        ~Specialist()
+        {
+            Console.WriteLine($"SDestructor\t:{this.GetHashCode()}");
+        }
+        public override void Info()
+        {
+            base.Info();
+            Console.WriteLine($"{Experience} {SpecialSkills} {Salary}");
+        }
+        public override string ToString()
+        {
+            return (base.ToString() + ":").Split('.').Last().PadRight(12)
+                + $"{Experience.ToString().PadRight(15)}{SpecialSkills.PadRight(15)}{Salary.ToString().PadRight(5)}";
+        }
+
+        public override string ToFileString()
+        {
+            return $"{GetType().ToString().Split('.').Last()}:{Experience},{SpecialSkills},{Salary}";
+        }
+        public override Human Init(string[] values)
+        {
+            base.Init(values);
+            this.Experience = Convert.ToDouble(values[8]);
+            this.SpecialSkills = values[9];
+            this.Salary = Convert.ToInt32(values[10]);
+            return this;
+        }
+    }
+}
